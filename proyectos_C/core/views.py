d09ea3f0d2d6731a0931172cf8f98e95f2c63c68
Pablo Fernández Guembe <fernandezpablo27@yahoo.com.ar>
2023-09-05 23:43:17 -0300

    descarga de archivos

--- proyectos_C/core/views.py
+++ proyectos_C/core/views.py
@@ -1,8 +1,29 @@
 from django.http import Http404
 from django.shortcuts import render, HttpResponse
-from .models import Project
-
+#from requests import Response
+from .models import Project, FilesAdmin
+import os
+from django.conf import settings
 
 def portfolio(request):
     projects = Project.objects.all()
     return render(request, "core/portfolio.html", {'projects': projects})
+
+
+def home(request):
+    # get all objects
+    OUR_CONTEXT={'file':FilesAdmin.objects.all()}
+    return render(request,'core/portfolio.html',OUR_CONTEXT)
+
+
+
+
+def download(request, path):
+    # get the download path
+    download_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(download_path):
+        with open(download_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/adminupload")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(download_path)
+            return response
+    raise Http404
--- proyectos_C/proyectos_C/urls.py
+++ proyectos_C/proyectos_C/urls.py
@@ -17,13 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.urls import re_path
 from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
     path("admin/", admin.site.urls),
+    re_path(r'^download/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
