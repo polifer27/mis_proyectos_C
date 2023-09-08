ba42f8df58385e8aa7e4412ea92e9a45b167d35f
Pablo Fernández Guembe <fernandezpablo27@yahoo.com.ar>
2023-09-08 12:10:09 -0300

    retoque del css

--- proyectos_C/core/models.py
+++ proyectos_C/core/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name ="Título")
     description = models.TextField(verbose_name ="Descripción")
     image = models.ImageField(verbose_name ="Imagen", upload_to="projects")
-    adminupload = models.FileField(upload_to='media')
+    adminupload = models.FileField(upload_to='media', default='media')
     created = models.DateTimeField(auto_now_add=True, verbose_name ="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name ="Fecha de edición")
     
